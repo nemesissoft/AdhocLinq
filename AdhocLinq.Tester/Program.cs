c6d2005c77027be3c5ca37d82f65a665d49de397
Michał Bryłka <michal.brylka@gmail.com>
2023-02-17 13:24:04 +0100

    Add Build.props. Modernize code

---
+++ AdhocLinq.Tester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using AdhocLinq;
+
+
+ParseDates();
+
+
+
+void ParseDates()
+{
+    var dynExp = DynamicExpressionFactory.DefaultFactory.Create();
+
+    var parsed = dynExp.Parse(typeof(string), """
+DateTime.Today.ToString("yyyyMMdd", CultureInfo.InvariantCulture) + " DESC " + (DateTime.Today + TimeSpan.FromHours(1)).ToString("HHmmss.ffff", CultureInfo.InvariantCulture)
+"""
+    );
+
+    var lambda = Expression.Lambda<Func<string>>(parsed);
+    var getter = lambda.Compile();
+
+    Console.WriteLine(getter());
+}
